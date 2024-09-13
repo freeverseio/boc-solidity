af1432f2aa6967f3570b8090af49a5caf70c3c72
Toni <>
2024-09-13 17:52:52 +0200

    adding code

--- contracts/BattleOfChains.sol
+++ contracts/BattleOfChains.sol
@@ -14,6 +14,36 @@ contract BattleOfChains is Ownable {
 
     constructor() Ownable(msg.sender) {}
 
+    function generateRandom() public view returns(uint256) {
+        return uint256(blockhash(block.number - 1));
+    }
+
+    // currently only the 1st two types are supported
+    function generateType(uint256 _random) public pure returns(uint256) {
+        return _random % 2;
+    }
+
+    function generateSlot(uint256 _random, uint256 _type) public pure returns (uint96 _slot) {
+        return uint96((_random << 3) | _type);
+    }
+
+    function presetTokenURI(uint256 _type) public pure returns (string memory) {
+        if (_type == 0) return "ipfs://QmType0";
+        if (_type == 1) return "ipfs://QmType1";
+        // Revert with a custom error message if type is not supported
+        revert("Type not supported");
+    }
+
+
+    // Doubts: add a _to to mint, so you can mint for other people too?
+
+    function mint(uint256 _joinedChainId) public returns (uint256 _tokenId) {
+        uint256 _random = generateRandom();
+        uint256 _type = generateType(_random);
+        uint96 _slot = generateSlot(_random, _type);
+        return collectionContract.mintWithExternalURI(msg.sender, _slot, presetTokenURI(_type));
+    }
+
     function mintWithExternalURI(
         address _to,
         uint96 _slot,
