ace29dadf3261b4a9d306dd12d9ef956cb4264c0
Toni <>
2024-10-08 17:17:33 +0200

    moving methods around

--- contracts/BattleOfChains.sol
+++ contracts/BattleOfChains.sol
@@ -45,16 +45,6 @@ contract BattleOfChains is Ownable {
         return _multichainMint(_homeChain, _type);
     }
 
-    function _joinChainIfNeeded(uint32 _homeChain) internal {
-        if (_homeChain == 0) revert HomeChainMustBeGreaterThanZero();
-        uint32 prevJoinedChain = homeChainOfUser[msg.sender];
-        if (prevJoinedChain == 0) {
-            _joinChain(_homeChain);
-        } else {
-            if (prevJoinedChain != _homeChain) revert UserAlreadyJoinedChain(msg.sender, prevJoinedChain);
-        }
-    }
-
     function multichainMint(uint32 _type) public returns (uint256 _tokenId) {
         uint32 _homeChain = homeChainOfUser[msg.sender];
         if (_homeChain == 0) revert UserHasNotJoinedChainYet(msg.sender);
@@ -73,6 +63,16 @@ contract BattleOfChains is Ownable {
         if (homeChainOfUser[msg.sender] != 0) revert UserAlreadyJoinedChain(msg.sender, homeChainOfUser[msg.sender]);
         homeChainOfUser[msg.sender] = _homeChain;
         emit JoinedChain(msg.sender, _homeChain);
+    }
+
+    function _joinChainIfNeeded(uint32 _homeChain) internal {
+        if (_homeChain == 0) revert HomeChainMustBeGreaterThanZero();
+        uint32 prevJoinedChain = homeChainOfUser[msg.sender];
+        if (prevJoinedChain == 0) {
+            _joinChain(_homeChain);
+        } else {
+            if (prevJoinedChain != _homeChain) revert UserAlreadyJoinedChain(msg.sender, prevJoinedChain);
+        }
     }
 
     function typeTokenURI(uint256 _type) public pure returns (string memory) {
