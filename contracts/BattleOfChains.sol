9e867878006dc4f426f482e0d3c029c176edfa0c
Toni <>
2024-10-10 15:25:48 +0200

    starting to add comments to all methods

--- contracts/BattleOfChains.sol
+++ contracts/BattleOfChains.sol
@@ -32,34 +32,32 @@ contract BattleOfChains is Ownable, IBattleOfChains, URIManager, SupportedContra
         collectionContract = IEvolutionCollection(_laosCollectionAddress);
     }
 
+    /// @inheritdoc IBattleOfChains
     function joinChain(uint32 _homeChain) public {
-        if (_homeChain == 0) revert HomeChainMustBeGreaterThanZero();
+        if (_homeChain == NULL_CHAIN) revert HomeChainMustBeGreaterThanZero();
         if (homeChainOf[msg.sender] != NULL_CHAIN) revert UserAlreadyJoinedChain(msg.sender, homeChainOf[msg.sender]);
         homeChainOf[msg.sender] = _homeChain;
         emit JoinedChain(msg.sender, _homeChain);
     }
 
+    /// @inheritdoc IBattleOfChains
     function multichainMint(uint32 _type) public returns (uint256 _tokenId) {
         uint32 _homeChain = homeChainOf[msg.sender];
         if (_homeChain == NULL_CHAIN) revert UserHasNotJoinedChainYet(msg.sender);
         return _multichainMint(_homeChain, _type);
     }
 
+    /// @inheritdoc IBattleOfChains
     function proposeChainAction(ChainAction calldata _chainAction, string calldata _comment) public {
         _proposeChainAction(msg.sender, _chainAction, _comment);
     }
 
+    /// @inheritdoc IBattleOfChains
     function proposeChainActionOnBehalfOf(address _user, ChainAction calldata _chainAction, string calldata _comment) public {
         _proposeChainAction(_user, _chainAction, _comment);
     }
 
-    function _proposeChainAction(address _user, ChainAction calldata _chainAction, string calldata _comment) private {
-        uint32 _sourceChain = homeChainOf[_user];
-        if (_sourceChain == NULL_CHAIN) revert UserHasNotJoinedChainYet(_user);
-        if (!areChainActionInputsCorrect(_chainAction)) revert IncorrectAttackInput();
-        emit ChainActionProposal(msg.sender, _user, _sourceChain, _chainAction, _comment);
-    }
-
+    /// @inheritdoc IBattleOfChains
     function areChainActionInputsCorrect(ChainAction calldata _chainAction) public pure returns (bool _isOK) {
         bool _isAttackAddressNull = _chainAction.attackAddress == address(0);
         bool _isAttackAreaNull = _chainAction.attackArea == Attack_Area.NULL;
@@ -73,6 +71,13 @@ contract BattleOfChains is Ownable, IBattleOfChains, URIManager, SupportedContra
         return _isTargetChainNull &&_isAttackAddressNull && _isAttackAreaNull;
     }
 
+    function _proposeChainAction(address _user, ChainAction calldata _chainAction, string calldata _comment) private {
+        uint32 _sourceChain = homeChainOf[_user];
+        if (_sourceChain == NULL_CHAIN) revert UserHasNotJoinedChainYet(_user);
+        if (!areChainActionInputsCorrect(_chainAction)) revert IncorrectAttackInput();
+        emit ChainActionProposal(msg.sender, _user, _sourceChain, _chainAction, _comment);
+    }
+
     function _multichainMint(uint32 _homeChain, uint32 _type) private returns (uint256 _tokenId) {
         uint96 _slot = uint96(uint256(blockhash(block.number - 1)));
         _tokenId = collectionContract.mintWithExternalURI(msg.sender, _slot, typeTokenURI(_type));
@@ -80,10 +85,12 @@ contract BattleOfChains is Ownable, IBattleOfChains, URIManager, SupportedContra
         return _tokenId;
     }
 
+    /// @inheritdoc IBattleOfChains
     function creatorFromTokenId(uint256 _tokenId) public pure returns(address _creator) {
         return address(uint160(_tokenId));
     }
 
+    /// @inheritdoc IBattleOfChains
     function coordinatesOf(address _user) public pure returns (uint256 _x, uint256 _y) {
         uint160 user160 = uint160(_user);
 
@@ -91,10 +98,12 @@ contract BattleOfChains is Ownable, IBattleOfChains, URIManager, SupportedContra
         _y = uint256(user160 & ((1 << 80) - 1));
     }
 
+    /// @inheritdoc IBattleOfChains
     function attack(uint256[] calldata _tokenIds, address _targetAddress, uint32 _targetChain, uint32 _strategy) public {
         _attack(_tokenIds, _targetAddress, msg.sender, _targetChain, _strategy);
     }
 
+    /// @inheritdoc IBattleOfChains
     function attackOnBehalfOf(uint256[] calldata _tokenIds, address _targetAddress, uint32 _targetChain, uint32 _strategy, address _attacker) public {
         _attack(_tokenIds, _targetAddress, _attacker, _targetChain, _strategy);
     }
@@ -103,6 +112,7 @@ contract BattleOfChains is Ownable, IBattleOfChains, URIManager, SupportedContra
         emit Attack(tokenIds, _targetAddress, msg.sender, _attacker, _targetChain, _strategy);
     }
 
+    /// @inheritdoc IBattleOfChains
     function hasHomeChain(address _user) public view returns(bool) {
         return homeChainOf[_user] != NULL_CHAIN;
     }
--- contracts/IBattleOfChains.sol
+++ contracts/IBattleOfChains.sol
@@ -52,24 +52,75 @@ interface IBattleOfChains {
     );
 
 
+    /**
+     * @notice Assigns the sender of the TX to the provided chain. Assignment cannot be modified.
+     * @dev Reverts if called more than once. ChainId = 0 is not allowed.
+     * @param _homeChain the chainId of the chain to assign to the sender
+     */
     function joinChain(uint32 _homeChain) external;
 
+    /**
+     * @notice Returns true if provided user has previously joined a chain
+     * @param _user the address of the user
+     */
     function hasHomeChain(address _user) external returns (bool);
 
+    /**
+     * @notice Mints NFTs accross all supported chains of the provided _type
+     * @notice The attributes of the minted NFT depend on the current state of the user and the game
+     * @param _type a uint32 specifying the desired type of minted NFTs 
+     * @return _tokenId the tokenId that identifies the minted NFTs in the contracts on all supported chains
+     */
     function multichainMint(uint32 _type) external returns (uint256 _tokenId);
 
+    /**
+     * @notice Votes for the homeChain of the sender to perform the provided chain action during
+     * @notice the current voting period. The effect of the vote depends on the current state of the user and the game
+     * @param _chainAction the desired action to be performed by the chain of the sender in the current period
+     * @param _comment a completely optional string arguing about the convenience of the proposed action
+     */
     function proposeChainAction(ChainAction calldata _chainAction, string calldata _comment) external;
 
+    /**
+     * @notice Tries to perform proposeChainAction on behalf of the provided user address.
+     * @notice The effect is completely disregarded offchain unless the user has previously authorized the
+     * @notice transaction sender as operator.
+     * @notice If not disregarded, it votes for the homeChain of the provided user to perform the provided chain action during
+     * @notice the current voting period. The effect of the vote depends on the current state of the user and the game
+     * @param _chainAction the desired action to be performed by the chain of the provided user in the current period
+     * @param _comment a completely optional string arguing about the convenience of the proposed action
+     */
     function proposeChainActionOnBehalfOf(address _user, ChainAction calldata _chainAction, string calldata _comment) external;
 
+    /**
+     * @notice Returns true if the provided chainAction is formally correct
+     * @param _chainAction the desired action to be performed by the chain of the provided user in the current period
+     * @return _isOK true if the provided chainAction is formally correct
+     */
     function areChainActionInputsCorrect(ChainAction calldata _chainAction) external pure returns (bool _isOK);
 
+    /**
+     * @notice Returns true if provided user has previously joined a chain
+     * @param _tokenId the address of the user
+     */
     function creatorFromTokenId(uint256 _tokenId) external pure returns(address _creator);
 
+    /**
+     * @notice Returns true if provided user has previously joined a chain
+     * @param _user the address of the user
+     */
     function coordinatesOf(address _user) external pure returns (uint256 _x, uint256 _y);
 
+    /**
+     * @notice Returns true if provided user has previously joined a chain
+     * @param _tokenIds the address of the user
+     */
     function attack(uint256[] calldata _tokenIds, address targetAddress, uint32 _targetChain, uint32 _strategy) external;
 
+    /**
+     * @notice Returns true if provided user has previously joined a chain
+     * @param _tokenIds the address of the user
+     */
     function attackOnBehalfOf(uint256[] calldata _tokenIds, address targetAddress, uint32 _targetChain, uint32 _strategy, address _attacker) external;
 
 }
