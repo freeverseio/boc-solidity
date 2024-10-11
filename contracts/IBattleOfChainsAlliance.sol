6051a5347f2ebbab1424c7d7e441e94f9ce59021
Toni <>
2024-10-11 13:53:30 +0200

    improving comments

--- contracts/IBattleOfChainsAlliance.sol
+++ contracts/IBattleOfChainsAlliance.sol
@@ -2,32 +2,23 @@
 pragma solidity >=0.8.27;
 
 /**
- * @title IBattleOfChainsOperator Interface
- * @dev Interface for the Battle of Chains contract for ownership chains
+ * @title IBattleOfChainsAlliance Interface
+ * @dev A general alliance can be an arbitrarily complex contract
+ * @dev It is strongly recommended that alliances implement the minimal logic
+ * @dev that this interface contains: assigning operators on LAOS, and responding 
+ * @dev to alliance name and alliance description.
  * @notice Developed and maintained by the LAOS Team and Freeverse.
  */
 
 interface IBattleOfChainsAlliance {
     /**
-     * @notice Grants permissions to the provide operator address on the LAOS Network to perform
-     * @notice game actions in the Battle of Chains game, such as attack, chain action proposals, etc.
-     * @notice Note that the entire 721 logic remains unaffectd by this assignment.
-     * @notice In particular, trading of all NFTs remains on-chain in this chain and contract.
-     * @notice The granted permission can be changed as many times as desired.
-     * @notice The granted permission can be revoked by simple calling this method with _operator = msg.sender.
-     * @dev This method needs not store any variable. It just emits the event, for offchain processing.
+     * @notice Returns the name of the alliance
      * @param name the name of the alliance
      */
     function allianceName() external returns (string memory name);
 
     /**
-     * @notice Grants permissions to the provide operator address on the LAOS Network to perform
-     * @notice game actions in the Battle of Chains game, such as attack, chain action proposals, etc.
-     * @notice Note that the entire 721 logic remains unaffectd by this assignment.
-     * @notice In particular, trading of all NFTs remains on-chain in this chain and contract.
-     * @notice The granted permission can be changed as many times as desired.
-     * @notice The granted permission can be revoked by simple calling this method with _operator = msg.sender.
-     * @dev This method needs not store any variable. It just emits the event, for offchain processing.
+     * @notice Returns a human readable description of the alliance
      * @param description the name of the alliance
      */
     function allianceDescription() external returns (string memory description);
