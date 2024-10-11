43a245d44ebd6ca4185a4106ee35fe53d41cb2c0
Toni <>
2024-10-11 17:29:52 +0200

    starting to deal with new collection

--- deployWithTruffle/scripts/deploy.js
+++ deployWithTruffle/scripts/deploy.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 const BattleOfChains = artifacts.require("BattleOfChains");
+const EvolutionCollectionFactory = artifacts.require("EvolutionCollectionFactory");
 
 const assignedCollectionAddress = "0xfFFfFffffFffFFfFFffffffe000000000000000D";
 
@@ -10,6 +11,11 @@ module.exports = async (callback) => {
     console.log('Deployer = ', deployer);  
     const aliceBalance = await web3.eth.getBalance(deployer);
     console.log('Deployer\'s balance:', web3.utils.fromWei(aliceBalance, 'ether'), 'LAOS');
+
+    console.log('Connecting to collection factory...');
+    const newcol = await EvolutionCollectionFactory.at("0x0000000000000000000000000000000000000403");
+    console.log('...DONE');
+      return;
 
     console.log('deploying BattleOfChains... assigned to collection ', assignedCollectionAddress);
     const battle = await BattleOfChains.new(assignedCollectionAddress);
