83b62bd74c529d1637323c9581b3e72a9c81cddb
Toni <>
2024-10-11 16:57:48 +0200

    adding first tokenURIs

--- deployWithTruffle/scripts/deploy.js
+++ deployWithTruffle/scripts/deploy.js
@@ -1,21 +1,24 @@
+/* eslint-disable no-undef */
 const BattleOfChains = artifacts.require("BattleOfChains");
-const truffleAssert = require('truffle-assertions');
 
 const assignedCollectionAddress = "0xfFFfFffffFffFFfFFffffffe000000000000000D";
-const maxGas = 10000000;
 
 module.exports = async (callback) => {
   try {
-    // Alice must be the owner of the current precompile collection
-    const [alice] = await web3.eth.getAccounts();
-    console.log('Alice = ', alice);  
-    const aliceBalance = await web3.eth.getBalance(alice);
-    console.log('Alice\'s balance:', web3.utils.fromWei(aliceBalance, 'ether'), 'LAOS');
+    // Deployer must be the owner of the current precompile collection
+    const [deployer] = await web3.eth.getAccounts();
+    console.log('Deployer = ', deployer);  
+    const aliceBalance = await web3.eth.getBalance(deployer);
+    console.log('Deployer\'s balance:', web3.utils.fromWei(aliceBalance, 'ether'), 'LAOS');
 
     console.log('deploying BattleOfChains... assigned to collection ', assignedCollectionAddress);
     const battle = await BattleOfChains.new(assignedCollectionAddress);
-    console.log('...deployed at ', battle.address);
+    console.log('...DONE. Deployed at ', battle.address);
     
+    console.log('assigning tokenURI for type 0 and type 1 multimints...', assignedCollectionAddress);
+    await battle.addTokenURIs([0, 1], ["ipfs://QmS5adNn3aAWVLLBXmjG3kK8gbq36NfnefmKm9udFhGi3K", "ipfs://QmS5adNn3aAWVLLBXmjG3kK8gbq36NfnefmKm9udFhGi3K"]);
+    console.log('...DONE');
+
     callback();
   } catch (error) {
     console.log(error);
