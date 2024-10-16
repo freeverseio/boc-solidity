07c66f32ceebfc85ddbdf0cf70ccbf8a58c0d576
Toni <>
2024-10-16 17:19:01 +0200

    added test to alliance

--- test/BattleOfChainsAlliance.test.js
+++ test/BattleOfChainsAlliance.test.js
@@ -21,12 +21,35 @@ describe("BattleOfChainsAlliance", function () {
   it("deploy sets storage as expected", async function () {
     expect(await battleOfChainsAlliance.allianceName()).to.equal(allianceName);
     expect(await battleOfChainsAlliance.allianceDescription()).to.equal(description);
+    expect(await battleOfChainsAlliance.battleOfChains721Contract()).to.equal(await battleOfChains721.getAddress());
   });
 
   it("assignOperator emits expected event, with emitter = alliance contract address", async function () {
     await expect(battleOfChainsAlliance.assignOperator(addr1.address))
       .to.emit(battleOfChains721, "AssignOperator")
       .withArgs(battleOfChainsAlliance.getAddress(), addr1.address);
+  });
+
+  it("should emit ShareTreasury event with absolute method", async function () {
+    const shareTXs = [
+      { recipient: addr1.address, amount: 100000 },
+      { recipient: owner.address, amount: 200000 }
+    ];
+
+    await expect(battleOfChainsAlliance.shareTreasuryAbsolute(shareTXs))
+    .to.emit(battleOfChains721, "ShareTreasury")
+    .withArgs(battleOfChainsAlliance.getAddress(), 0, [[addr1.address, 100000],[owner.address, 200000]]);
+  });
+
+  it("should emit ShareTreasury event with percentage method", async function () {
+    const shareTXs = [
+      { recipient: addr1.address, amount: 10 },
+      { recipient: owner.address, amount: 20 }
+    ];
+
+    await expect(battleOfChainsAlliance.shareTreasuryPercentage(shareTXs))
+    .to.emit(battleOfChains721, "ShareTreasury")
+    .withArgs(battleOfChainsAlliance.getAddress(), 1, [[addr1.address, 10],[owner.address, 20]]);
   });
 });
 
